b5a0206daf2b35e7dea5beeddc4665512052cfce
jordanpeter <jordanpeter26@gmail.com>
2018-02-16 09:21:51 +0100

    save

---
+++ tests/menus.php
@@ -0,0 +1,10 @@
+<nav id="menu">
+    <div class="element_menu">
+        <h3>Titre menu</h3>
+        <ul>
+            <li><a href="../tests/fonctions.php">Lien</a></li>
+            <li><a href="../tests/test.php">Lien</a></li>
+
+        </ul>
+    </div>
+</nav>
